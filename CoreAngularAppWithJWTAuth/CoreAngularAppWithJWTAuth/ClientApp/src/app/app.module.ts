5b37142eab975c920d9f89a91e120cae951dfc7c
sunil singh <sunilsingh77@gmail.com>
2020-03-18 20:39:57 +0530

    done login component page

--- CoreAngularAppWithJWTAuth/CoreAngularAppWithJWTAuth/ClientApp/src/app/app.module.ts
+++ CoreAngularAppWithJWTAuth/CoreAngularAppWithJWTAuth/ClientApp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
@@ -29,6 +29,7 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
--- CoreAngularAppWithJWTAuth/CoreAngularAppWithJWTAuth/ClientApp/src/app/login/login.component.ts
+++ CoreAngularAppWithJWTAuth/CoreAngularAppWithJWTAuth/ClientApp/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../services/account.service';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  insertForm: FormGroup;
+  Username: FormControl;
+  Password: FormControl;
+  returnUrl: string;
+  ErrorMessage: string;
+  invalidLogin: boolean;
+
+  constructor(private acct: AccountService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private fb: FormBuilder) { }
 
   ngOnInit() {
-  }
+    this.Username = new FormControl('', [Validators.required]);
+    this.Password = new FormControl('', [Validators.required]);
 
+    // get return URL or /
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+
+    // Intialize formGroup by using FormBuilder
+    this.insertForm = this.fb.group({
+      'Username': this.Username,
+      'Password': this.Password
+    });
+  }
+  onSubmit() {
+    const userLogin = this.insertForm.value;
+    this.acct.login(userLogin.Username, userLogin.Password).subscribe(result => {
+      const token = (<any>result).token;
+      console.log(token);
+      console.log(result.returnUrl);
+      console.log('User logged-in successfully!');
+      console.log(result.userRole);
+      this.invalidLogin = false;
+      this.router.navigateByUrl(this.returnUrl);
+    },
+      error => {
+        this.invalidLogin = true;
+        this.ErrorMessage = 'Invalid logged-in details supplied - Could not logged-In!';
+        console.log(this.ErrorMessage);
+      });
+  }
 }

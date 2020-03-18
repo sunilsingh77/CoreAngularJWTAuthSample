bdbf10e6594bb561f31a4dbb73c8b40c46f16318
sunil singh <sunilsingh77@gmail.com>
2020-03-18 13:00:31 +0530

    test

--- CoreAngularAppWithJWTAuth/CoreAngularAppWithJWTAuth/ClientApp/src/app/app-routing.module.ts
+++ CoreAngularAppWithJWTAuth/CoreAngularAppWithJWTAuth/ClientApp/src/app/app-routing.module.ts
@@ -16,7 +16,7 @@ const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: 'products', component: ProductsComponent },
-    { path: '**', redirectTo: '/home' }
+    { path: '**', redirectTo: '/home' } 
 ];
 
 @NgModule({
